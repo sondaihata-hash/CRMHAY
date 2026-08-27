7d163701c242e24accf3ebc2549f4a6f72208cc2
nguyencaoky1121-dev <nguyencaoky1121@gmail.com>
2026-08-28 05:48:21 +0700

    chore: auto-save changes 2026-08-28 05:48:21

--- tests/test_zalo_handoff.py
+++ tests/test_zalo_handoff.py
@@ -1,6 +1,6 @@
 import uuid
 
-from app import Customer, SalesGroup, app, db
+from app import Customer, SalesGroup, SalesHandoff, app, db
 
 
 def test_zalo_handoff_returns_configured_group_link():
@@ -27,6 +27,7 @@ def test_zalo_handoff_returns_configured_group_link():
         payload = response.get_json()
         assert payload['destination_url'] == 'https://zalo.me/g/example-group'
 
+        db.session.query(SalesHandoff).filter_by(customer_id=customer.id).delete()
         db.session.delete(group)
         db.session.delete(customer)
         db.session.commit()
